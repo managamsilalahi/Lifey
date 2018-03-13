6357f03b4dad0bbbb47fa6cc902656443ed05ab0
Managam Silalahi <managam.silalahi@gmail.com>
2018-03-13 22:03:28 +0700

    Initial commit

---
+++ Lifey/Lifey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lifey
+//
+//  Created by Managam Silalahi on 13/03/18.
+//  Copyright © 2018 Managam's Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
